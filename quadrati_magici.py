d3ed38c95e9f9a7b1aed306700b06bc02e9f189a
MatteoBorgio <matteo6.borgio@gmail.com>
2025-02-18 17:39:31 +0100

    aggiunta la verifica della matrice per le colonne e le diagonali

--- quadrati_magici.py
+++ quadrati_magici.py
@@ -13,26 +13,30 @@ def genera_matrice(n: int, valid_numbers: list):
         matrice.append(row)
     return matrice
 
-def verifica_quadrato_magico(matrice):
+def verifica_quadrato_magico(matrice: list[list[int]]):
     """Verifica se la matrice è un quadrato magico."""
-    row_result = []
-    counter = 0
-    for riga in matrice:
-        for elemento in riga:
-            counter += elemento
-        row_result.append(counter)
-        counter = 0
-    for i in range(len(row_result)):
-        if row_result[i]!= row_result[0]:
+    for i in range(len(matrice)):
+        if sum(matrice[i])!= sum(matrice[0]):
             return False
+    for col in range(len(matrice)):
+        col_result = 0
+        for row in matrice:
+            col_result += row[col]
+        if col_result!= sum(matrice[0]):
+            return False
+    primary_diag_result = 0
+    secondary_diag_result = 0
+    for i in range(len(matrice)):
+        primary_diag_result += matrice[i][i]
+        secondary_diag_result += matrice[i][len(matrice) - 1 - i]
+    if primary_diag_result!= sum(matrice[0]) or secondary_diag_result != sum(matrice[0]):
+        return False
     return True
 
-def stampa_matrice(matrice, costante_magica=None):
+def stampa_matrice(matrice: list[list[int]]):
     """Stampa la matrice in un formato leggibile, aggiungendo la costante se è  disponibile."""
     for i in range(len(matrice)):
-        for j in range(len(matrice[i])):
-            print(matrice[i][j])
-        print("\n")
+        print(matrice[i])
 
 def main():
     """Funzione principale che genera e verifica quadrati magici."""
@@ -44,7 +48,10 @@ def main():
             break
         else:
             continue
-    return f"{matrice} è un quadrato perfetto"
+    print("Il quadrato magico generato è:")
+    stampa_matrice(matrice)
+    costante_magica = sum(matrice[0])
+    print(f"La costante magica del quadrato è: {costante_magica}")
 
 if __name__ == "__main__":
-    print(main())
+    main()
